cf380a88381f19538f09e45aeec7bccfae3fb9b1
SichDylan <sichdylann@gmail.com>
2026-01-06 16:33:28 +0100

    DragonBall

---
+++ controller/homeController.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>Dragon ball ...</h1>
+    </header>
+
+    <main>
+
+        <h2>Select a character</h2>
+
+        <div class="character-list">
+
+            <div class="character-card 1 selected ">
+                <p class="character-name">Goku</p>
+            </div>
+
+            <div class="character-card 2 ">
+                <p class="character-name">Freezer</p>
+            </div>
+
+            <div class="character-card 3">
+                <p class="character-name">Cell</p>
+            </div>
+
+            <div class="character-card 4">
+                <p class="character-name">Pikachu</p>
+            </div>
+
+            <div class="character-card 5">
+                <p class="character-name">Mohamed</p>
+            </div>
+
+        </div>
+
+        <div class="choose-button">Choose "Selected Caracter"</div>
+
+    </main>
+
+    <footer>
+            <p>J'aime pas dragon ball</p>
+    </footer>
+</body>
+
+</html>
