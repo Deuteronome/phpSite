fd1a1dd54516263996d0eaec291196ed14783868
habac1 (remplacer par pseudo utilisé pour le compte gitHub) <janos.mat7@proton.me (remplacer par adresse mail utilisz pour le compte gitHub)>
2026-01-12 16:25:26 +0100

    first template-mcv stucture

---
+++ controller/characterController.php
@@ -0,0 +1,36 @@
+<?php
+if ($_GET["character"] == 'goku') {
+    $title = "Sangoku - the Saiyan";
+    $asideImg = "../asset/gokuSide.gif";
+} elseif ($_GET["character"] == 'freezer') {
+    $title = "Freezer - the icy alien";
+    $asideImg = "../asset/freezer.gif";
+} else {
+    header("location:../controller/homeController.php");
+
+}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personnage</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="../script/title.js" defer></script>
+</head>
+<body>
+    <?php
+    require_once("../view/_asisde.php");
+?>
+
+    <div id="right">
+        <?php
+            require_once("../view/_header.php");
+        ?>      
+</body>
+</html>
---
+++ view/homeView.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <script src="../script/title.js" defer></script>
+</head>
+
+<body>
+<?php
+    require_once("../view/_asisde.php");
+?>
+
+    <div id="right">
+        <?php
+            require_once("../view/_header.php");
+        ?>
+                
+        <main>
+
+            <h2>Select a character</h2>
+
+            <div class="character-list">
+
+                <a href="../controller/characterController.php?character=goku" class="character-card" id="goku" data-name="Goku">
+                    
+                <p class="character-name">Goku</p>
+                </a>
+
+                <a href="../controller/characterController.php?character=freezer" class="character-card" id="freezer" data-name="Freezer" >
+
+                <p class="character-name">Freezer</p>
+                </a>
+
+                <div class="character-card" id="cell" data-name="Cell">
+                    
+                <p class="character-name">Cell</p>
+                </div>
+
+                <div class="character-card" id="pikachu" data-name="Pikachu">
+                    
+                <p class="character-name">Pikachu</p>
+                </div>
+
+                <div class="character-card" id="mohamed" data-name="Mohamed">
+
+                <p class="character-name">Mohamed</p>
+                </div>
+
+            </div>
+
+
+        </main>
+
+    </div>
+    
+</body>
+
+
+</html>
