a69959388ab1c9c81927cdf72f85583ca0dae018
Deuteronome <o.burcker@e2c-grandlille.fr>
2026-01-28 09:28:39 +0100

    Boutons fiche personnage

--- view/characterView.php
+++ view/characterView.php
@@ -17,14 +17,20 @@
         ?>
 
     <main>
-        <div class="character-sheet">
-            <p class="desc"><?= $fighter->getDescription() ?></p>
-            <p class="stat"><?= $fighter->getPower() ?></p>
-            <p class="stat"><?= $fighter->getResistance() ?></p>
-            <p class="stat"><?= $fighter->getChaos() ?></p>
-            <p class="stat"><?= $fighter->getHitpoints() ?></p>
-        </div> 
-        </main>
+        <div class="sheet-wrapper">
+            <div class="character-sheet">
+                <p class="desc"><?= $fighter->getDescription() ?></p>
+                <p class="stat">Puissance : <?= $fighter->getPower() ?></p>
+                <p class="stat">Défense : <?= $fighter->getResistance() ?></p>
+                <p class="stat">Chaos : <?= $fighter->getChaos() ?></p>
+                <p class="stat">Points de vie : <?= $fighter->getHitpoints() ?></p>
+            </div>
+            <div class="choice-zone">
+                <a href="../controller/cardGame.php" class="btn">Valider</a>
+                <a href="../controller/homeController.php" class="btn">Retour</a>
+            </div>
+        </div>
+    </main>
 
     </div>
 </body>
